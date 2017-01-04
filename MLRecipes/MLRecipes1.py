e72dc20afc41fba00d2843f52223dcd7bdf565cf
Emir Hodzic <emir.hodzic@compusight.com>
2017-01-04 15:39:54 +0100

    This is the first commit

---
+++ MLRecipes/MLRecipes1.py
@@ -0,0 +1,8 @@
+#https://www.youtube.com/playlist?list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2ryal
+from sklearn import tree
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[160,0]]))
